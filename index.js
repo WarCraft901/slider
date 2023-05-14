0e317d14efe4167c694a9727199b8da966eb37b4
WarCraft901 <xikmatillayev901@gmail.com>
2023-05-14 23:09:13 +0300

    slider

---
+++ index.js
@@ -0,0 +1,50 @@
+const imgs = document.getElementById('imgs');
+const leftBtn = document.getElementById('left');
+const rightBtn = document.getElementById('right');
+
+const img = document.querySelectorAll('#imgs img');
+
+let idx = 0;
+
+function changeImage(){
+    if(idx>img.length - 1) {
+        idx=0;
+    } else if(idx<0){
+        idx = img.length -1;
+    }
+    imgs.style.transform = `translateX(${-idx * 500}px)`;
+
+}
+
+
+rightBtn.addEventListener('click',function change(){
+
+    idx ++;
+
+    changeImage()
+
+});
+
+rightBtn.addEventListener('keydown',(e)=>{
+ if(e.key=='d'){
+    idx ++;
+
+    changeImage();
+ }
+});
+
+leftBtn.addEventListener('click',function change(){
+
+    idx --;
+
+    changeImage();
+
+});
+
+leftBtn.addEventListener('keydown',(e)=>{
+    if(e.key=='a'){
+       idx --;
+   
+       changeImage();
+    }
+   });
